b1ccc5305377368fe7cf85b4f64bd6999703f8a5
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-08-03 09:49:36 -0600

    Added AppStyles.xaml

--- MauiWeather/MauiProgram.cs
+++ MauiWeather/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MauiWeather
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Rubik-Light.ttf", "RubikLight");
+                    fonts.AddFont("Rubik-Regular.ttf", "Rubik");
                 });
 
 #if DEBUG
