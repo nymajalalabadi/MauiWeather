f0a1fdcd90a4aff8b2ef46154696ec39eddbaf0c
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-08-03 10:53:22 -0600

    Added SearchCommand

--- MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
+++ MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiWeather.MVVM.ViewModels
 {
     public class WeatherViewModel
     {
+        public ICommand SearchCommand => new Command<string>(async (searchBar) =>
+        {
+            if (!string.IsNullOrWhiteSpace(searchBar))
+            {
+                Location location = await GetCoordinatesAsync(searchBar.ToString());
+                
+            }
+        });
+        private async Task<Location> GetCoordinatesAsync(string address)
+        {
+            IEnumerable<Location> locations = await Geocoding.Default.GetLocationsAsync(address);
+
+            Location location = locations?.FirstOrDefault();
+
+            if (location != null)
+            {
+                Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+            }
+
+            return location;
+        }
+
     }
 }
