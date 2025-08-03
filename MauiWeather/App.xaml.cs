a2573d0d07cc3a9e5e21655c57aab24bfc165641
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-08-03 09:26:40 -0600

    Add project files.

---
+++ MauiWeather/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MauiWeather.MVVM.Views;
+
+namespace MauiWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new WeatherView());
+        }
+    }
+}
